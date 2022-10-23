907c46248943723816643113e0aae61fb6110ac9
bprafulkumar <prafulkumar176@gmail.com>
2022-10-23 15:34:50 +0530

    displayed the names and small change in fetch

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,17 @@
 import React, {useState} from "react";
 import MainpageLayout from "../components/MainpageLayout";
+import { apiget } from "../misc/config";
 
 const Home = () => {
 
     const [input , SetInput] = useState("")
+    const [results , setResults] = useState(null);
     
     const onSearch = () => {
-        fetch(`https://api.tvmaze.com/search/shows?q=${input}`).then((res) => res.json()).then((result) => console.log(result))
+        apiget(`/search/shows?q=${input}`).then((result) => 
+        setResults(result))
+        // fetch(`https://api.tvmaze.com/search/shows?q=${input}`).then((res) => res.json()).then((result) => 
+        // setResults(result))
     }
 
     const onInputChange = (e) => {
@@ -14,16 +19,30 @@ const Home = () => {
     }
 
     const onKeyDown = (ev) => {
-        console.log(ev.keyCode)
+        // console.log(ev.keyCode)
         if(ev.keyCode === 13){
             onSearch()
         }
+    }
+
+    const renderItems = () => {
+        if(results && results.length === 0){
+            return (<div>NO RESULTS</div>);
+        }
+
+        if(results && results.length > 0){
+                return <div>{results.map((items)=><div key={items.show.id}>{items.show.name}</div>)}</div>
+        }
+        return null
     }
     return(
         <div>
            <MainpageLayout> THIS IS THE HOME PAGE </MainpageLayout>
             <input type="text" onChange={onInputChange} value={input} onKeyDown={onKeyDown} ></input>
-            <button onClick={onInputChange}>SEARCH</button>
+            <button type="button" onClick={onInputChange}>SEARCH</button>
+           {
+            renderItems()
+           }
         </div>
 
     )
