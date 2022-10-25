406d874b3171916fac15c63c70dc64fec502f538
bprafulkumar <prafulkumar176@gmail.com>
2022-10-25 12:41:46 +0530

    styles added to show page

--- src/components/show/showMainData.js
+++ src/components/show/showMainData.js
@@ -1,32 +1,32 @@
 import React from 'react';
-
+import { MainDataWrapper,Headline , TagList } from './showMainData.styled';
 import IMG_PLACEHOLDER from '../../images/not-found.png';
 import { Star } from '../styled';
 
 const ShowMainData = (props) => {
   return (
-    <div>
+    <MainDataWrapper>
       <img src={props.image ? props.image.original : IMG_PLACEHOLDER} alt="show-cover" />
-      <div>
-        <div>
+      <div className='text-side '>
+        <Headline>
           <h1>{props.name}</h1>
           <div>
             <Star />
             <span>{props.rating.average || 'N/A'}</span>
           </div>
-        </div>
+        </Headline>
         <div dangerouslySetInnerHTML={{ __html: props.summary }} />
 
-        <div>
+        <TagList>
           Tags:{' '}
           <div>
             {props.tags.map((tag, i) => (
               <span key={i}>{tag}</span>
             ))}
           </div>
-        </div>
+        </TagList>
       </div>
-    </div>
+    </MainDataWrapper>
   );
 };
 export default ShowMainData;
